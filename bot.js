0b4dd137c0250af3ef843d5ad2c8195fe39de494
George Blackwell <geo.wmb@gmail.com>
2019-11-04 21:36:30 +0000

    fixed port issue... again

--- bot.js
+++ bot.js
@@ -1,20 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
-const PORT = process.env.PORT || 3000;
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-    if (msg.content.toLowerCase() === 'hello dan') {
-        msg.reply('hello there');
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
     }
 });
 
-client.login(auth.token);
-
-client.addListener(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+client.login(process.env.BOT_TOKEN);
