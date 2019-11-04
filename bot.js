32e832a8d04a6f0e8ee64f2d9f5a253aef3d22d7
George Blackwell <geo.wmb@gmail.com>
2019-11-04 21:42:31 +0000

    clean up

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+import { Client } from 'discord.js';
+
+const client = new Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'hello dan') {
+        msg.reply('hello there');
     }
 });
 
