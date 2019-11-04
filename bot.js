61af77bde2adc2e7c3c92b53b2b660000db24cda
George Blackwell <geo.wmb@gmail.com>
2019-11-04 20:20:35 +0000

    fixed port issue

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
 client.on('message', msg => {
     if (msg.content.toLowerCase() === 'hello dan') {
         msg.reply('hello there');
@@ -14,3 +13,6 @@ client.on('message', msg => {
 });
 
 client.login(auth.token);
+
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
