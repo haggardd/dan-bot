4958b61cd47b7d3bf309b47291fcc5cea1b01504
George Blackwell <geo.wmb@gmail.com>
2019-11-06 20:34:03 +0000

    improved command logic

--- bot.js
+++ bot.js
@@ -1,20 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const commmandPrefix = '-dan';
+
+client.on('warn', console.warn);
+client.on('error', console.error);
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`dan-bot logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    var msgContent = msg.content.toLowerCase();
+client.on('disconnect', () => {
+    console.log('dan-bot disconnected!');
+});
 
-    if (msgContent === 'hello') {
-        msg.reply('hello there');
+client.on('reconnecting', () => {
+    console.log('dan-bot is reconnecting!');
+});
+
+client.on('message', message => {
+    var messageContent = message.content.toLowerCase().trim();
+
+    if(messageContent.startsWith(commmandPrefix))
+    {
+        if (messageContent === commmandPrefix + 'hello') {
+            message.reply('hello there');
+        }
+    
+        if (messageContent === commmandPrefix + 'avatar') {
+            message.reply(message.author.avatarURL);
+        }
     }
-
-    if (msgContent === 'avatar') {
-        msg.reply(msg.author.avatarURL);
-      }
 });
 
 client.login(process.env.BOT_TOKEN);
